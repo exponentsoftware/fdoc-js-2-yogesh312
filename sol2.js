4b0f1979bfc2546af39a0339db5baf5d003e69aa
yogesh <etpr.yogesh@gmail.com>
2021-09-19 02:16:48 +0530

    fn for adding a user if doesnt exist

---
+++ sol2.js
@@ -0,0 +1,52 @@
+const users = [
+    {
+        name:'Brook', 
+        scores:75,
+        skills:['HTM', 'CSS', 'JS'],
+        age:16
+    },
+    {
+        name:'Alex', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    }, 
+    {
+        name:'David', 
+        scores:75,
+        skills:['HTM', 'CSS'],
+        age:22
+    }, 
+    {
+        name:'John', 
+        scores:85,
+        skills:['HTM'],
+        age:25
+    },
+    {
+        name:'Sara',
+        scores:95,
+        skills:['HTM', 'CSS', 'JS'],
+        age: 26
+    },
+    {
+        name:'Martha', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    },
+    {
+        name:'Thomas',
+        scores:90,
+        skills:['HTM', 'CSS', 'JS'],
+        age:20
+    }
+];
+//adding user only if user doesnt exit
+function addUser(name,score,skills,age){
+    if(!users.find(element=>element.name==name)){
+        users.push({name:name, scores:score, skills:skills, age:age})
+    }
+    console.log(users)
+}
+addUser('y',100,["fullstack"],26);
